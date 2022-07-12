40d23a6e4766d3fc73ca725baad26a60ff2c1933
p-328 <69773020+p-328@users.noreply.github.com>
2022-07-11 23:24:54 -0400

    Update index.php
    Refactored code to make it look nicer.

--- create-account/index.php
+++ create-account/index.php
@@ -1,30 +1,28 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<title>Create Account</title>
+		<link rel="stylesheet" href="../assets/styles.css" type="text/css">
+		<link rel="icon" href="../assets/WeatherMan!.png">
+	</head>
 
-<head>
-	<title>Create Account</title>
-	<link rel="stylesheet" href="../assets/styles.css" type="text/css">
-	<link rel="icon" href="../assets/WeatherMan!.png">
-</head>
-
-<body class='bg'>
-	<div class='center'>
-		<img src="../assets/WeatherMan!.png" class="logo">
-		<h1>Create Account</h1>
-		<form action="create.php" method="GET">
-			<label>Username</label>
-			<br>
-			<input type="text" name="username" class="input-form-login" required>
-			<br>
-			<br>
-			<label>Password</label>
-			<br>
-			<input type="password" name="password" class="input-form-login" required>
-			<br>
-			<br>
-			<input type="submit" class="btn-submit-creation" value="Create account!">
-		</form>
-	</div>
-</body>
-
+	<body class='bg'>
+		<div class='center'>
+			<img src="../assets/WeatherMan!.png" class="logo">
+			<h1>Create Account</h1>
+			<form action="create.php" method="GET">
+				<label>Username</label>
+				<br>
+				<input type="text" name="username" class="input-form-login" required>
+				<br>
+				<br>
+				<label>Password</label>
+				<br>
+				<input type="password" name="password" class="input-form-login" required>
+				<br>
+				<br>
+				<input type="submit" class="btn-submit-creation" value="Create account!">
+			</form>
+		</div>
+	</body>
 </html>
